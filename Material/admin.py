171232cff0374ffabc6504298c0280514b2a4731
gohyuhan <gohyuhan12345@gmail.com>
2023-06-20 15:04:19 +0800

    admin.py update

--- Material/admin.py
+++ Material/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
+from .models import Material, MaterialQuantity, MaterialStock
+
 # Register your models here.
+admin.site.register(Material)
+admin.site.register(MaterialQuantity)
+admin.site.register(MaterialStock)
