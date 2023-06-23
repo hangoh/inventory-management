89045427d0fbeea6949afe41c859096955e2a850
gohyuhan <gohyuhan12345@gmail.com>
2023-06-23 17:09:26 +0800

    dropping middleware to use authentication for store UUID checking

--- IM_server/settings.py
+++ IM_server/settings.py
@@ -57,8 +57,7 @@ MIDDLEWARE = [
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'Store.middleware.CheckStoreMiddleware'
+    'django.middleware.clickjacking.XFrameOptionsMiddleware'
 ]
 
 REST_FRAMEWORK = {
